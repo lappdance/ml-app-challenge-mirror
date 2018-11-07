0ddb9751775f12627246dccdc3c913bb27a20b30
Jason Lapp <jason@vectorthis.com>
2018-11-06 20:54:58 -0500

    Added a CircleCI file
    File copied from https://circleci.com/docs/2.0/language-android/ and https://github.com/googlesamples/android-sunflower/blob/master/.circleci/config.yml

---
+++ app/src/test/java/com/lappdance/mlappchallenge/SanityTest.java
@@ -0,0 +1,12 @@
+package com.lappdance.mlappchallenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SanityTest {
+    @Test
+    public void sanity() {
+        assertTrue(true);
+    }
+}
