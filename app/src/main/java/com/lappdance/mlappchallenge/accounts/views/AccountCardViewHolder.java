92ef755daa8beb6621ccf55678029c9d18550702
Jason Lapp <jason@vectorthis.com>
2018-11-11 21:34:59 -0500

    Show negative account balances in red

--- app/src/main/java/com/lappdance/mlappchallenge/accounts/views/AccountCardViewHolder.java
+++ app/src/main/java/com/lappdance/mlappchallenge/accounts/views/AccountCardViewHolder.java
@@ -1,5 +1,8 @@
 package com.lappdance.mlappchallenge.accounts.views;
 
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -27,6 +30,20 @@ public class AccountCardViewHolder extends RecyclerView.ViewHolder {
 
     public void setBalance(double balance) {
         this.balance.setText(Utils.formattedCurrency(balance));
+
+        final Context context = this.balance.getContext();
+        final @ColorRes int balanceColour;
+        if (balance < 0) {
+            balanceColour = R.color.negativeBalance;
+        } else {
+            final int[] attrs = {android.R.attr.textColor};
+            TypedArray ta = context.obtainStyledAttributes(attrs);
+            balanceColour = ta.getResourceId(0, android.R.color.black);
+
+            ta.recycle();
+        }
+
+        this.balance.setTextColor(context.getResources().getColor(balanceColour));
     }
 
     public void setNumber(@NonNull String formattedNumber) {
